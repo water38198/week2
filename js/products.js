178b16fd77a00430496ff9a015715e772967a404
DESKTOP-I4TOG67\water <water38198@gmail.com>
2023-01-08 14:21:28 +0800

    1.加入check api檢查使用者是否登入 2.移除多餘console.log

--- js/products.js
+++ js/products.js
@@ -9,30 +9,37 @@ const app = {
         };
     },
     created() {
+        this.myToken = document.cookie.replace(
+            /(?:(?:^|.*;\s*)myToken\s*\=\s*([^;]*).*$)|^.*$/,
+            "$1"
+        );
+        axios.defaults.headers.common["Authorization"] = this.myToken;
         this.isLogin();
     },
     methods: {
         isLogin() {
-            this.myToken = document.cookie.replace(
-                /(?:(?:^|.*;\s*)myToken\s*\=\s*([^;]*).*$)|^.*$/,
-                "$1"
-            );
-            console.log(this.myToken);
-            if (!this.myToken) {
-                alert("請先登入");
-                location.href = "index.html";
-            } else {
-                this.getProductData();
-            }
+            axios
+                .post(`${url}/v2/api/user/check`)
+                .then((res) => {
+                    const isSuccess = res.data.success;
+                    if (isSuccess) {
+                        this.getProductData();
+                    } else {
+                        alert("請先登入");
+                        location.href = "index.html";
+                    }
+                })
+                .catch((err) => {
+                    alert("請先登入");
+                    console.log(err);
+                    location.href = "index.html";
+                });
         },
         getProductData() {
-            axios.defaults.headers.common["Authorization"] = this.myToken;
             axios
                 .get(`${url}/v2/api/${api_path}/admin/products`)
                 .then((res) => {
-                    console.log(res.data.products);
                     this.productList = res.data.products;
-                    console.log(this.productList);
                 })
                 .catch((err) => {
                     console.log(err);
